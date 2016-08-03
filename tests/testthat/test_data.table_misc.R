c489c0b2074fe8bdfac3c586eb4fb8feec917637
Mateusz Dziedzic <mat.dziedzic+git@gmail.com>
2016-08-03 10:42:54 +0100

    Tests for count_NAs

--- tests/testthat/test_data.table_misc.R
+++ tests/testthat/test_data.table_misc.R
@@ -13,3 +13,27 @@ test_that("move_columns is working as expected", {
   expect_equal(move_columns(dt, "g first"),  dt[, .(g, a, b)])
   expect_equal(move_columns(dt, "g first; a last"), dt[, .(g, b, a)])
 })
+
+test_that("count_NAs is working as expected", {
+dt <- data.table::data.table(a = rnorm(n=1000, mean=20, sd=5),
+                             b = rnorm(n=1000, mean=20, sd=5),
+                             c = rnorm(n=1000, mean=20, sd=5))
+dt[sample.int(nrow(dt), round(nrow(dt) * 0.15)), a := NA_real_]
+
+expected_1 <- data.table::data.table(Variable = c("a", "b", "c"),
+                                     Count = c(150, 0, 0))
+expected_2 <- data.table::data.table(Variable = c("a", "b", "c"),
+                                     Count = c(150, 0, 0),
+                                     Prop = c(15, 0, 0))
+expected_3 <- data.frame(Count = 150, row.names = "Variable")
+expected_4 <- data.frame(Count = 150, Prop = 15, row.names = "Variable")
+expected_5 <- data.table::data.table(Variable = "a", Count = 150)
+expected_6 <- data.table::data.table(Variable = "a", Count = 150, Prop = 15)
+
+expect_equal(count_NAs(dt, print = FALSE), expected_1)
+expect_equal(count_NAs(dt, prop = TRUE, print = FALSE), expected_2)
+expect_equal(count_NAs(dt[, a], print = FALSE), expected_3)
+expect_equal(count_NAs(dt[, a], prop = TRUE, print = FALSE), expected_4)
+expect_equal(count_NAs(dt[, .(a)], print = FALSE), expected_5)
+expect_equal(count_NAs(dt[, .(a)], prop = TRUE, print = FALSE), expected_6)
+})

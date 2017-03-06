bf7958c202b798c7a00df6c7f2365ae260de29ba
Mateusz Dziedzic <mat.dziedzic+git@gmail.com>
2017-03-06 13:17:57 +0000

    setup_proxy and rm_all_exc_func initial commit

--- R/misc.R
+++ R/misc.R
@@ -1,51 +1,76 @@
 
-#' Title
+#' Setup proxy settings
 #'
-#' @param path
-#' @param str_to_match
+#' @param url      Proxy url
+#' @param port     Proxy port
+#' @param username Proxy username
+#' @param password Proxy password
 #'
-#' @return
+#' @return \code{TRUE}
 #' @export
-#' @import data.table
-#' @importFrom stringr str_detect
+#' @importFrom httr reset_config set_config use_proxy
+#' @importFrom getPass getPass
 #'
 #' @examples
-#' path <- mdmisc::get_active_file_path()
-#' mdmisc::merge_csv(path, 'string to match')
-merge_csv <- function(path, str_to_match, suffix = '_merged') {
-  # Helper function
-  fread_fwrite <- function(file_name, append = FALSE) {
-    fwrite(
-      fread(file.path(path, file_name), na.strings = c('','NA')),
-      file = file.path(path, paste0(str_to_match, '.csv')), append = append)
+#' mdmisc::setup_proxy() # Runs with default file_path
+setup_proxy <- function(file_path = 'F:/Marketing/Group Marketing/Database Marketing/Marketing Analytics/Personal/Mateusz/00 DandG proxy settings/proxy_settings.csv',
+                        url = NULL, port = NULL, username = NULL, password = getPass::getPass()) {
+  if(!file.exists(file_path) & (is.null(url) | is.null(port) | is.null(username))) stop('Please provide a valid file_path or proxy settings.')
+  if(file.exists(file_path)) {
+    setup_values <- read.csv(file_path, stringsAsFactors = FALSE)
+    url = setup_values[1, 'url']
+    port = setup_values[1, 'port']
+    username = setup_values[1, 'username']
   }
-  process <- TRUE
-  # List csv files and check for input and output matches
-  csv_files <- list_csv_files(path)
-  csv_files <- csv_files[str_detect(csv_files, str_to_match)]
-  if (length(csv_files) <= 1) stop('Single or no matching files, quitting.')
-  in_match <- sapply(csv_files, function(x) {x == paste0(str_to_match, '.csv')})
-  out_match <- sapply(csv_files, function(x) {x == paste0(str_to_match, '_merged.csv')})
-  if (any(in_match)) {
-    message(paste0('File \'', paste0(str_to_match, '.csv\''), ' exactly matching input pattern, omit?'))
-    input <- yes_no_input()
-    if (input %in% c('y','yes','Yes')) {
-      message('Omitting exactly matching file.')
-      csv_files <- csv_files[!in_match]
-    }}
-  if (any(out_match)) {
-    message(paste0('File \'', paste0(str_to_match, '_merged.csv\''), ' exists, overwrite?'))
-    input <- yes_no_input()
-    if (input %in% c('n','no','No')) {
-      message('Quitting without overwriting.')
-      process <- FALSE
-    }}
-  # Process
-  if(process) {
-    fread_fwrite(csv_files[1], append = FALSE)
-    lapply(csv_files[-1], function(x) fread_fwrite(x, append = TRUE))
-    return(TRUE)
-  } else return(FALSE)
+  if(is.null(url) | is.null(port) | is.null(username)) {
+    missing_args <- c('url', 'port', 'username')[c(is.null(url), is.null(port), is.null(username))]
+    stop(paste0('Arguments: ', paste(missing_args, collapse = ', '), ' missing.'))
+  }
+  message(paste0('Setting up proxy with the following parameters: url = ', url, ', port = ', port, ', username = ', username))
+  reset_config()
+  set_config(
+    use_proxy(url, port, username, password)
+  )
+  return(TRUE)
+}
+
+#' Remove all user defined variables except functions
+#'
+#' @param ask \code{logical} indicating whether user confirmation is needed.
+#'
+#' @return None (invisible NULL).
+#' @export
+#'
+#' @examples
+#' rm_all_exc_func()  # Will ask for confirmation.
+rm_all_exc_func <- function(ask = TRUE) {
+  yn <- TRUE
+  if(ask) {
+    user_input <- yes_no_input('Removing all user defined variables except functions, continue?')
+    yn <- is.yes(user_input)
+  }
+  if(yn) rm(
+    list = setdiff(
+      ls(envir = parent.frame()),
+      lsf.str(envir = parent.frame())),
+    envir = parent.frame()) else message('Quitting without removal.')
+}
+
+#' Simple wrapper for \code{pacman::p_load}
+#'
+#' Function name matches the first characters of \code{library}
+#'
+#' @param package
+#'
+#' @return None (invisible NULL).
+#' @export
+#' @importFrom pacman p_load
+#'
+#' @examples
+#' lib_pacman(ggplot2)
+#' lib_pacman('ggplot2')
+lib_pacman <- function(package) {
+  p_load(char = as.character(substitute(package)))
 }
 
 #' Formats \code{POSIXct} (e.g. \code{Sys.time()}) object to YYYYMMDD_HHMMSS string

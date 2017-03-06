790e027001a364b5079e5e13864921e9f6f75e98
Mateusz Dziedzic <mat.dziedzic+git@gmail.com>
2017-03-06 13:22:32 +0000

    message_text added

--- R/internals.R
+++ R/internals.R
@@ -4,9 +4,12 @@
 #' @return User input if matching one of \code{c('y','yes','Yes','n','no','No')}
 #'
 #' @examples
-yes_no_input <- function() {
+yes_no_input <- function(message_text = NULL) {
   repeat{
+    if(is.not.null(message_text)) message(message_text)
     message(paste0('Please enter (y)es/(n)o:'))
     input <- readline()
     if(input %in% c('y','yes','Yes','n','no','No')) return(input)
   }}
+
+is.yes <- function(yn) { yn %in% c('y','yes','Yes') }

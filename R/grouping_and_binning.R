d2b8eecfa73149dcca30989aa9c1ffbe98be0121
Mateusz Dziedzic <mat.dziedzic+git@gmail.com>
2017-03-07 17:39:07 +0000

    Fixed thresholds

--- R/grouping_and_binning.R
+++ R/grouping_and_binning.R
@@ -14,19 +14,31 @@
 #' @export
 #'
 #' @examples
+#' library(mdmisc)
 #' set.seed(2016)
 #' dt <- data.table::data.table('cat' = sample(letters[1:10], 20, replace = TRUE), 'cont' = rnorm(20))
 #'
-#' # View cumulative frequencies
+#' ## View cumulative frequencies
 #' group_by_threshold(dt, 'cat')
+#' group_by_threshold(dt, 'cat', threshold = 0.10)
+#' group_by_threshold(dt, 'cat', cum_threshold = 0.90)
+#' group_by_threshold(dt, 'cat', no_of_categories = 3)
 #'
-#' # Group bottom 20\% categories based on cumulative frequency
-#' dt_mod <- group_by_threshold(dt, 'cat', cum_threshold = 0.2, return_data = TRUE)
-#' dt_mod
+#' ## Group categories below 10\% of frequency
+#' dt_mod <- group_by_threshold(dt, 'cat', threshold = 0.10, return_data = TRUE)
+#' group_by_threshold(dt_mod, 'cat')
 #'
-#' # Group and modify in place
-#' group_by_threshold(dt, 'cat', cum_threshold = 0.2, return_data = TRUE, modify = TRUE)
-#' dt
+#' ## Group bottom 10\% categories based on cumulative frequency
+#' dt_mod <- group_by_threshold(dt, 'cat', cum_threshold = 0.90, return_data = TRUE)
+#' group_by_threshold(dt_mod, 'cat')
+#'
+#' ## Leave 3 categories based on frequency
+#' dt_mod <- group_by_threshold(dt, 'cat', no_of_categories = 3, return_data = TRUE)
+#' group_by_threshold(dt_mod, 'cat')
+#'
+#' ## Group and modify in place
+#' group_by_threshold(dt, 'cat', threshold = 0.1, return_data = TRUE, modify = TRUE)
+#' group_by_threshold(dt, 'cat')
 group_by_threshold <- function(dt,
                                feature,
                                threshold = NULL,
@@ -63,23 +75,23 @@ group_by_threshold <- function(dt,
   freq_tab <- dt[, list(N = .N), by = feature][order(-N)]
   freq_tab[, PCT := N / sum(N)][, CumPCT := cumsum(PCT)]
   # Prepare feature names
-  top_cats <- freq_tab[, get(feature)]
-  # Categories not to be collapsed
-  if(!is.null(threshold))        top_cats <- top_cats[freq_tab[, PCT]    <= (threshold)]
-  if(!is.null(cum_threshold))    top_cats <- top_cats[freq_tab[, CumPCT] <= (1 - cum_threshold)]
-  if(!is.null(no_of_categories)) top_cats <- top_cats[1:no_of_categories]
+  cats_to_collapse <- freq_tab[, get(feature)]
+  # Categories to be collapsed
+  if(!is.null(threshold))        cats_to_collapse <- cats_to_collapse[freq_tab[, PCT]    <= (threshold)]
+  if(!is.null(cum_threshold))    cats_to_collapse <- cats_to_collapse[freq_tab[, CumPCT] >= (cum_threshold)]
+  if(!is.null(no_of_categories)) cats_to_collapse <- cats_to_collapse[-(1:no_of_categories)]
   # Group categories and return if return_data is true
   if (return_data) {
     if (!modify) {
       dt_out <- copy(dt)
-      return(dt_out[!(get(feature) %in% top_cats), c(feature) := other_cat_name])
+      return(dt_out[(get(feature) %in% cats_to_collapse), c(feature) := other_cat_name])
     } else {
-      dt[!(get(feature) %in% top_cats), c(feature) := other_cat_name]
+      dt[(get(feature) %in% cats_to_collapse), c(feature) := other_cat_name]
       return(dt)
     }
   } else {
-    if(mdmisc::is.not.null(threshold))             return(freq_tab[, ToGroup := !(PCT    <= (threshold))][])
-    else if(mdmisc::is.not.null(cum_threshold))    return(freq_tab[, ToGroup := !(CumPCT <= (1 - cum_threshold))][])
+    if(mdmisc::is.not.null(threshold))             return(freq_tab[, ToGroup := (PCT    <= (threshold))][])
+    else if(mdmisc::is.not.null(cum_threshold))    return(freq_tab[, ToGroup := (CumPCT >= (cum_threshold))][])
     else if(mdmisc::is.not.null(no_of_categories)) return(freq_tab[, ToGroup := c(rep(FALSE, no_of_categories), rep(TRUE, .N - no_of_categories))][])
     else return(freq_tab)
   }

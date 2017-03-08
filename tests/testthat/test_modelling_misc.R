25ae7ebbcdeb1c395f42a5dff61e9301b3f1226b
Mateusz Dziedzic <mat.dziedzic+git@gmail.com>
2017-03-08 10:54:30 +0000

    Tests for gini_norm

--- tests/testthat/test_modelling_misc.R
+++ tests/testthat/test_modelling_misc.R
@@ -1,6 +1,12 @@
 
-context("Test modelling_misc.R functions from mdmisc package")
+context('Test modelling_misc.R functions from mdmisc package')
 
-test_that("check_features_match_glm is working as expected", {
-  # TODO: Write tests...
+test_that('gini_norm is equal 2 * AUC - 1', {
+  df <- data.frame(
+   actual = c(1,1,1,1,1,0,1,1,1,0,1,1,0,1,1,1,1,0,0,0,1,0,1,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0),
+   predicted = seq(1, 0, length.out = 40)
+  )
+  gini_val <- gini_norm(df[, 'actual'], df[, 'predicted'])
+  AUC_val <- pROC::roc(df[, 'actual'], df[, 'predicted'])$auc
+  expect_equal(gini_val, 2 * AUC_val - 1)
 })
